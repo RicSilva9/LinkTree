3b22338f230a09ae94a7f2a359474c2c66145037
rssricardo82@gmail.com <rssricardo82@gmail.com>
2023-12-18 21:37:36 -0300

    coloquei os backgrounds mobile

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle("light")
-
-    const img = document.querySelector("#profile img")
-    if(html.classList.contains('light')) {
-        img.setAttribute('src', './IMG-perfil/Foto-Perfil-light.jpg')
-    } else {
-        img.setAttribute('src', './IMG-perfil/Foto-Perfil.jpg')
-    }
 }
